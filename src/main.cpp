340fbb8255a8304d478d928d88c6c91e4e865ffe
Frachop <franck.chopin@gmail.com>
2018-06-13 15:36:23 +0200

    initial import

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  submoduletest
+//
+//  Created by Frachop on 13/06/2018.
+//  Copyright © 2018 Ymagis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
